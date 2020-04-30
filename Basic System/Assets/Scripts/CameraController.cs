d2854314a77cb37b877bfc72447506cb5c3cbd0d
sniper-link <sniper-link@users.noreply.github.com>
2020-04-30 14:41:43 -0700

    Camara Control v0.1 4/30/20

---
+++ Basic System/Assets/Scripts/CameraController.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    const float CAM_Y_MIN = 10f;
+    const float CAM_Y_MAX = 60f;
+
+    float camCurX = 0;
+    float camCurY = 10;
+
+    float rotSpeedX = 8;
+    float rotSpeedY = 5;
+
+    public float distance = 10;
+    public Transform playerTrans;
+    Camera playerCam;
+    Transform cameraTrans;
+    SphereCollider camCollider;
+
+    private void Awake()
+    {
+        playerCam = this.GetComponent<Camera>();
+        cameraTrans = playerCam.transform;
+        camCollider = this.GetComponent<SphereCollider>();
+        Cursor.visible = false; // hide mouse cursor
+        Cursor.lockState = CursorLockMode.Locked; // Bound cursor with in the screen
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        camCurX += (Input.GetAxis("Mouse X") * rotSpeedX);
+        camCurY -= (Input.GetAxis("Mouse Y") * rotSpeedY);
+
+        camCurY = Mathf.Clamp(camCurY, CAM_Y_MIN, CAM_Y_MAX);
+    }
+
+    void LateUpdate()
+    {
+        Vector3 dir = new Vector3(0, 0, -distance);
+        Quaternion rotation = Quaternion.Euler(camCurY, camCurX, 0);
+        cameraTrans.position = playerTrans.position + rotation * dir;
+        cameraTrans.LookAt(playerTrans.position);
+    }
+}
---
+++ Basic System/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private Camera playerCamera;
+    private Animator animController;
+    private CharacterController characterController;
+
+    public float rotateSpeed = 10;
+    public float moveSpeed = 1;
+    public float gravity = 20;
+    public float jumpForce = 8;
+
+    Vector3 moveDir;
+
+    private void Awake()
+    {
+        animController = transform.GetComponent<Animator>();
+        characterController = transform.GetComponent<CharacterController>();
+        if (playerCamera == null)
+        {
+            playerCamera = transform.Find("MainCamera").GetComponent<Camera>();
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        animController.SetBool("IsGrounded", true);
+        characterController.stepOffset = 0.4f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveX = Input.GetAxis("Horizontal");
+        float moveZ = Input.GetAxis("Vertical");
+
+        Vector3 camForward;
+        Vector3 camRight;
+
+        camForward = playerCamera.transform.forward;
+        camRight = playerCamera.transform.right;
+
+        /*if (moveX != 0 && moveY !=0)
+        {
+            //print(moveX + "|" + moveY);
+            moveDir = transform.position + new Vector3(moveX*.1f, 0, moveY * .1f);
+        }*/
+
+        moveDir = new Vector3(((camForward.x * moveZ) + (camRight.x * moveX)) * moveSpeed, 0, ((camForward.z * moveZ) + (camRight.z * moveX)) * moveSpeed);
+
+        if (characterController.isGrounded && Input.GetButton("Jump"))
+        {
+            moveDir.y = jumpForce;
+            animController.SetBool("IsGrounded", characterController.isGrounded);
+        }
+
+        moveDir.y -= gravity * Time.deltaTime;
+
+        characterController.Move(moveDir);
+
+        moveDir = Vector3.zero;
+
+        animController.SetBool("IsGrounded", characterController.isGrounded);
+        animController.SetFloat("PlayerSpeed", moveDir.magnitude);
+        print(characterController.isGrounded);
+    }
+}
